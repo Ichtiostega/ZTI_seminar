991119be12c19856271daa57cb188effdc7a985e
Karol Kocierz <kkna102@gmail.com>
2021-05-16 16:18:15 +0200

    Initial

---
+++ src/main/java/com/zti/SeminarBootData/SeminarBootDataApplication.java
@@ -0,0 +1,13 @@
+package com.zti.SeminarBootData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeminarBootDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeminarBootDataApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/zti/SeminarBootData/model/Student.java
@@ -0,0 +1,57 @@
+package com.zti.SeminarBootData.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.GenerationType;
+
+@Entity
+public class Student {
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getAlbum_nr() {
+        return album_nr;
+    }
+
+    public void setAlbum_nr(Integer album_nr) {
+        this.album_nr = album_nr;
+    }
+
+    public Student(String name, String surname, Integer album_nr) {
+        this.name = name;
+        this.surname = surname;
+        this.album_nr = album_nr;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String surname;
+    private Integer album_nr;
+
+    protected Student(){}
+    
+}
