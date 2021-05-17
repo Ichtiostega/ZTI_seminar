dabab69ae8bc76ccbf8a658766d9f9bfb6d44f12
Wojciech Korzybski <wojciech.korzybski@gmail.com>
2021-05-17 19:39:43 +0200

    WIP

--- src/main/java/com/zti/SeminarBootData/api/RestApi.java
+++ src/main/java/com/zti/SeminarBootData/api/RestApi.java
@@ -4,13 +4,18 @@ import com.zti.SeminarBootData.dao.DatabaseInterface;
 import com.zti.SeminarBootData.model.Student;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@Transactional
 public class RestApi {
 
     @Autowired
@@ -30,6 +35,34 @@ public class RestApi {
     public @ResponseBody Iterable<Student> getStudentsByName(@PathVariable(value="surname") String surname) {
         return databaseInterface.findBySurname(surname);
     }
-    
+
+    @GetMapping(value="/startingSurname/{surname}")
+    public @ResponseBody Iterable<Student> findBySurnameStartingWith(@PathVariable(value="surname") String surname)
+    {
+        return databaseInterface.findBySurnameStartingWith(surname);
+    }
+
+    @GetMapping(value="/endingWith/{name}/{id}")
+    public @ResponseBody Iterable<Student> findByNameEndingWith(@PathVariable String name, @PathVariable int id)
+    {
+        return databaseInterface.findByNameEndingWithIgnoreCaseAndIdGreaterThanEqualOrderBySurnameDesc(name, id);
+    }
+
+    @GetMapping(value="/nieNull")
+    public @ResponseBody Iterable<Student> nieNull()
+    {
+        return databaseInterface.nieNull();
+    }
+
+    @DeleteMapping(value="/bySur/{surname}")
+    public void deleteBySurname(@PathVariable String surname){
+        databaseInterface.deleteBySurname(surname);
+    }
+
+    @PostMapping(value="/print")
+    public void postEntity(@RequestBody Student student)
+    {
+        System.out.println(student.getName() + student.getSurname());
+    }
 
 }
--- src/main/java/com/zti/SeminarBootData/dao/DatabaseInterface.java
+++ src/main/java/com/zti/SeminarBootData/dao/DatabaseInterface.java
@@ -2,10 +2,16 @@ package com.zti.SeminarBootData.dao;
 
 import com.zti.SeminarBootData.model.Student;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DatabaseInterface extends CrudRepository<Student, Integer> {
     Iterable<Student> findBySurname(String surname);
+    Iterable<Student> findBySurnameStartingWith(String surname);
+    Iterable<Student> findByNameEndingWithIgnoreCaseAndIdGreaterThanEqualOrderBySurnameDesc(String name, int id);
+    @Query(value = "select * from student s where s.name is not Null", nativeQuery=true)
+    Iterable<Student> nieNull();
+    void deleteBySurname(String surname);
 }
